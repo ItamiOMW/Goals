8993ecda0c96507d394540311bb6cd9260d231df
Вадим <itamiomw@gmail.com>
2022-12-25 18:08:52 +0300

    setup project

--- app/src/main/java/com/example/goals/ui/theme/Theme.kt
+++ app/src/main/java/com/example/goals/ui/theme/Theme.kt
@@ -1,21 +1,18 @@
 package com.example.goals.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val MainColorPalette = darkColors(
+    primary = GrayShade,
+    primaryVariant = GrayShadeLight,
+    secondary = PurpleLight,
+    onPrimary = TextWhite,
+
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
 
     /* Other default colors to override
     background = Color.White,
@@ -25,18 +22,13 @@ private val LightColorPalette = lightColors(
     onBackground = Color.Black,
     onSurface = Color.Black,
     */
-)
+
 
 @Composable
-fun GoalsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun GoalsTheme(content: @Composable () -> Unit) {
 
     MaterialTheme(
-        colors = colors,
+        colors = MainColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
--- app/src/main/java/com/example/goals/ui/theme/Type.kt
+++ app/src/main/java/com/example/goals/ui/theme/Type.kt
@@ -2,17 +2,28 @@ package com.example.goals.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.goals.R
+
+
+val fonts = FontFamily(
+    Font(R.font.gothica1_black, weight = FontWeight.Black),
+    Font(R.font.gothica1_regular, weight = FontWeight.Normal),
+    Font(R.font.gothica1_bold, weight = FontWeight.Bold),
+    Font(R.font.gothica1_medium, weight = FontWeight.Medium),
+    Font(R.font.gothica1_semibold, weight = FontWeight.SemiBold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
+    ),
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
