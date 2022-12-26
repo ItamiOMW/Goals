08608c7931b62d1e87cc58acf043d0bd1841e870
Вадим <itamiomw@gmail.com>
2022-12-26 13:20:13 +0300

    implemented tasks repository

--- app/src/main/java/com/example/goals/domain/models/Task.kt
+++ app/src/main/java/com/example/goals/domain/models/Task.kt
@@ -2,6 +2,7 @@ package com.example.goals.domain.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.goals.utils.UNKNOWN_ID
 
 @Entity(tableName = "tasks_table")
 data class Task(
@@ -10,5 +11,5 @@ data class Task(
     val color: Int,
     val scheduledDateTime: Long,
     @PrimaryKey(autoGenerate = true)
-    val id: Int? = null,
+    val id: Int = UNKNOWN_ID,
 )
