943fe3b630bea81f55bba1d57bab884dc56b8e89
Вадим <itamiomw@gmail.com>
2022-12-26 13:14:50 +0300

    implemented goals repository

--- app/src/main/java/com/example/goals/domain/models/Goal.kt
+++ app/src/main/java/com/example/goals/domain/models/Goal.kt
@@ -2,6 +2,7 @@ package com.example.goals.domain.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.goals.utils.UNKNOWN_ID
 
 @Entity(tableName = "goals_table")
 data class Goal(
@@ -13,5 +14,5 @@ data class Goal(
     val endTimestamp: Long,
     val color: Int,
     @PrimaryKey(autoGenerate = true)
-    val id: Int? = null,
+    val id: Int = UNKNOWN_ID,
 )
